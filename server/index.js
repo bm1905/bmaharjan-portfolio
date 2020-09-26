42245fc26c4689c051ee9fbc753ddb9b35d2c761
Bijay Maharjan <bijay.maharjan5@gmail.com>
2020-09-25 22:38:04 -0500

    SSL Config

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const sslRedirect = require('heroku-ssl-redirect');
 
 // Use this to access DB URL.
 const config = require('./config');
 
 const app = express();
+app.use(sslRedirect());
 app.use(bodyParser.json());
+
 
 if (process.env.NODE_ENV === 'production') {
     const appPath = path.join(__dirname, '..', 'build');
     app.use(express.static(appPath));
 
-    app.get('*', function(req, res) {
+    app.get('*', function (req, res) {
         res.sendFile(path.resolve(appPath, 'index.html'));
     });
 };
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`Server started at PORT ${PORT}!!`);
 });
