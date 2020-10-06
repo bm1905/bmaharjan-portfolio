c8e8220c21dc468f787e3ab5ae2a99e4db0dfacb
Bijay Maharjan <bijay.maharjan5@gmail.com>
2020-10-06 13:03:45 -0500

    SSL Fix

--- server/index.js
+++ server/index.js
@@ -3,19 +3,31 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+var forceSsl = function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    return next();
+};
+
+
+
 // Use this to access DB URL.
 const config = require('./config');
 
 const app = express();
 app.use(bodyParser.json());
 
+
+
+
 if (process.env.NODE_ENV === 'production') {
 
     const appPath = path.join(__dirname, '..', 'build');
     app.use(express.static(appPath));
 
+    app.use(forceSsl());
     app.get('*', function (req, res) {
-        app.use(sslRedirect());
         res.sendFile(path.resolve(appPath, 'index.html'));
     });
 };
@@ -25,3 +37,4 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, function () {
     console.log(`Server started at PORT ${PORT}!!`);
 });
+
