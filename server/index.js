cf15c8435ec3e7476d4d99683509d9891e0c94fd
Bijay Maharjan <bijay.maharjan5@gmail.com>
2020-10-06 13:15:43 -0500

    SSL Fix

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const sslRedirect = require('heroku-ssl-redirect');
+// const sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-var forceSsl = function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    return next();
-};
+// var forceSsl = function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//         return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//     }
+//     return next();
+// };
 
 
 
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(forceSsl());
+    // app.use(forceSsl());
     const appPath = path.join(__dirname, '..', 'build');
     app.use(express.static(appPath));
     app.get('*', function (req, res) {
